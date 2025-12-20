08cf901642b255eaf12c73a6d72d7d5fb540c71e
Chris Geoga <geoga@wisc.edu>
2025-12-20 10:20:31 -0600

    updates, starting a TODO list.

--- julia/interface.jl
+++ julia/interface.jl
@@ -1,17 +1,21 @@
 
-using StaticArrays
+using StaticArrays, BenchmarkTools
 
-# 2s versus 53s with HNSW.jl for 1M pts and k=30.
+const libsequentialknn = "../target/release/libsequentialknn.so"
+
 function sequentialknn_condix(pts::Vector{SVector{D,Float64}}, k) where{D}
   knn = zeros(UInt64, (k, length(pts)))
-  ccall((:sequential_knn, "../target/release/libsequentialknn.so"),
-        Cvoid,
-        (Ptr{UInt64}, Ptr{Float64}, Csize_t, Csize_t, Csize_t),
-        knn, pts, Csize_t(length(pts)), Csize_t(D), Csize_t(k))
+  @ccall libsequentialknn.sequential_knn(knn::Ptr{UInt64}, pts::Ptr{Float64},
+                                         length(pts)::Csize_t, D::Csize_t, 
+                                         k::Csize_t)::Cvoid
   map(enumerate(eachcol(knn))) do (j, cj)
     c = Int64.(cj)[1:min(j-1, k)]
     sort!(c)
     c
   end
 end
+
+# 2s versus 53s with HNSW.jl.
+const pts = rand(SVector{2,Float64}, 1_000_000)
+@btime sequentialknn_condix($pts, $(30));
 
