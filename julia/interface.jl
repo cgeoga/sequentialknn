bf7d3b72702e0a865555a99ce9948c2d4acaf71c
Chris Geoga <geoga@wisc.edu>
2026-01-01 11:25:53 -0600

    Adding a bucket size and runtime dispatch for it. But the performance really goes down as the bucket size increases, so maybe a different ultra-fast solution is necessary for (gappy) lattice data.

--- julia/interface.jl
+++ julia/interface.jl
@@ -3,11 +3,32 @@ using StaticArrays, BenchmarkTools
 
 const libsequentialknn = "../target/release/libsequentialknn.so"
 
+function required_bucket_size(pts::Vector{SVector{D,Float64}}) where{D}
+  maximum(1:D) do j
+    ptsj  = getindex.(pts, j)
+    sort!(ptsj)
+    dptsj = diff(ptsj)
+    (ix, n_equal) = (1, 1)
+    while true
+      new_ix  = findnext(!=(0.0), dptsj, ix)
+      isnothing(new_ix) && break
+      n_equal = max(n_equal, new_ix - ix + 1)
+      ix      = new_ix + 1
+    end
+    nextpow(2, n_equal)
+  end
+end
+
 function sequentialknn_condix(pts::Vector{SVector{D,Float64}}, k) where{D}
-  knn = zeros(UInt64, (k, length(pts)))
+  knn    = zeros(UInt64, (k, length(pts)))
+  bucket = max(32, required_bucket_size(pts))
+  if bucket > 4096
+    throw(error("The required bucket size for this collection of points is prohibitively large. Unfortunately, this accelerated library wrapper does not currently support your use case."))
+  end
+  @info "Using bucket size $bucket."
   @ccall libsequentialknn.sequential_knn(knn::Ptr{UInt64}, pts::Ptr{Float64},
                                          length(pts)::Csize_t, D::Csize_t, 
-                                         k::Csize_t)::Cvoid
+                                         bucket::Csize_t, k::Csize_t)::Cvoid
   map(enumerate(eachcol(knn))) do (j, cj)
     c = Int64.(cj)[1:min(j-1, k)]
     sort!(c)
@@ -15,7 +36,12 @@ function sequentialknn_condix(pts::Vector{SVector{D,Float64}}, k) where{D}
   end
 end
 
-# 2s versus 53s with HNSW.jl.
+print("n=1M random 2D, k=30:  ")
 const pts = rand(SVector{2,Float64}, 1_000_000)
-@btime sequentialknn_condix($pts, $(30));
+@btime sequentialknn_condix($pts, $(30)) # 2s, vs 53s with HNSW.jl.
+
+print("n=2^20 gridded 2D, k=30:  ")
+pts1d = range(0.0, 1.0, length=2^10)
+const pts = vec(SVector{2,Float64}.(Iterators.product(pts1d, pts1d)))
+@btime sequentialknn_condix($pts, $(30)) # 10s. Not great. 
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
-use kiddo::{KdTree, NearestNeighbour, SquaredEuclidean};
+use kiddo::{NearestNeighbour, SquaredEuclidean};
+use kiddo::float::kdtree::KdTree;
 use std::slice;
 
 #[unsafe(no_mangle)]
@@ -7,24 +8,50 @@ pub extern "C" fn sequential_knn(
     pts: *const f64, // flat array: n * d
     n: usize,
     d: usize,
+    b: usize,
     k: usize,
 ) {
-    match d {
-        2 => sequential_knn_static::<2>(buf, pts, n, k),
-        3 => sequential_knn_static::<3>(buf, pts, n, k),
-        4 => sequential_knn_static::<4>(buf, pts, n, k),
-        _ => panic!("This should not be reachable."),
+    match (d, b) {
+      // 2D:
+      (2, 32) => sequential_knn_static::<2,32>(buf, pts, n, k),
+      (2, 64) => sequential_knn_static::<2,64>(buf, pts, n, k),
+      (2, 128) => sequential_knn_static::<2,128>(buf, pts, n, k),
+      (2, 256) => sequential_knn_static::<2,256>(buf, pts, n, k),
+      (2, 512) => sequential_knn_static::<2,512>(buf, pts, n, k),
+      (2, 1024) => sequential_knn_static::<2,1024>(buf, pts, n, k),
+      (2, 2048) => sequential_knn_static::<2,2048>(buf, pts, n, k),
+      (2, 4096) => sequential_knn_static::<2,4096>(buf, pts, n, k),
+      // 3D:
+      (3, 32) => sequential_knn_static::<3,32>(buf, pts, n, k),
+      (3, 64) => sequential_knn_static::<3,64>(buf, pts, n, k),
+      (3, 128) => sequential_knn_static::<3,128>(buf, pts, n, k),
+      (3, 256) => sequential_knn_static::<3,256>(buf, pts, n, k),
+      (3, 512) => sequential_knn_static::<3,512>(buf, pts, n, k),
+      (3, 1024) => sequential_knn_static::<3,1024>(buf, pts, n, k),
+      (3, 2048) => sequential_knn_static::<3,2048>(buf, pts, n, k),
+      (3, 4096) => sequential_knn_static::<3,4096>(buf, pts, n, k),
+      // 4D:
+      (4, 32) => sequential_knn_static::<4,32>(buf, pts, n, k),
+      (4, 64) => sequential_knn_static::<4,64>(buf, pts, n, k),
+      (4, 128) => sequential_knn_static::<4,128>(buf, pts, n, k),
+      (4, 256) => sequential_knn_static::<4,256>(buf, pts, n, k),
+      (4, 512) => sequential_knn_static::<4,512>(buf, pts, n, k),
+      (4, 1024) => sequential_knn_static::<4,1024>(buf, pts, n, k),
+      (4, 2048) => sequential_knn_static::<4,2048>(buf, pts, n, k),
+      (4, 4096) => sequential_knn_static::<4,4096>(buf, pts, n, k),
+      // else:
+      _ => panic!("Your given combination of dimension and required bucket size is not supported. But this panic shouldn't be reachable from the Julia wrapper, so please open an issue."),
     }
 }
 
-fn sequential_knn_static<const D: usize>(buf: *mut u64, pts: *const f64, n: usize, k: usize) {
+fn sequential_knn_static<const D: usize, const B: usize>(buf: *mut u64, pts: *const f64, n: usize, k: usize) {
     // Reinterpret Vector{SVector{D,Float64}} as a flat array.
     let points: &[[f64; D]] = unsafe { slice::from_raw_parts(pts as *const [f64; D], n) };
     // Reinterpret output buffer, a 2D column-major (k,n) array, as a flat
     // array (so buf[(i, j)] = buf[j*k+ i]).
     let out: &mut [u64] = unsafe { slice::from_raw_parts_mut(buf, n * k) };
     // build the tree.
-    let mut tree: KdTree<f64, D> = KdTree::with_capacity(n);
+    let mut tree: KdTree<f64, u64, D, B, u32> = KdTree::with_capacity(n);
     // sequentially update the tree. Note that for the first few indices that
     // are <= k, some entries of buf will be left un-filled. But that will be
     // handled on the Julia side.
